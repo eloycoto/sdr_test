895d28eb7abdf5d348c48fbe86181f23fd484d48
Eloy Coto <eloy.coto@acalustra.com>
2024-12-28 13:54:54 +0100

    feat: added the reader and write to SPI device

--- pipewire_to_spi/src/bin/reader.rs
+++ pipewire_to_spi/src/bin/reader.rs
@@ -1,18 +1,49 @@
 use pipewire as pw;
+
 use pw::{properties::properties, spa};
+use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 use spa::param::format::{MediaSubtype, MediaType};
 use spa::param::format_utils;
 use spa::pod::Pod;
 use std::convert::TryInto;
+use std::io;
 use std::mem;
+
+struct SpiWriter {
+    spi: Spi,
+}
+
+impl SpiWriter {
+    // @TODO the SPI device should come from ARG, just to iterate faster
+    fn new() -> io::Result<Self> {
+        let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        Ok(SpiWriter { spi })
+    }
+
+    fn write(&mut self, data: &[u8]) -> io::Result<usize> {
+        self.spi
+            .write(data)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+    }
+}
+
 
 struct UserData {
     format: spa::param::audio::AudioInfoRaw,
     cursor_move: bool,
 }
 
+// @TODO should make an error enum here,
 pub fn main() -> Result<(), pw::Error> {
     pw::init();
+
+    //@TODO Need to add a fake Writer here and define a trait
+    let mut writer = match SpiWriter::new(){
+        Ok(x) => x,
+        Err(e) => panic!("Is not runnig on RPI {}", e)
+    };
 
     let mainloop = pw::main_loop::MainLoop::new(None)?;
     let context = pw::context::Context::new(&mainloop)?;
@@ -67,7 +98,7 @@ pub fn main() -> Result<(), pw::Error> {
                 user_data.format.channels()
             );
         })
-        .process(|stream, user_data| match stream.dequeue_buffer() {
+        .process(move |stream, user_data| match stream.dequeue_buffer() {
             None => println!("out of buffers"),
             Some(mut buffer) => {
                 let datas = buffer.datas_mut();
@@ -83,6 +114,8 @@ pub fn main() -> Result<(), pw::Error> {
                     if user_data.cursor_move {
                         print!("\x1B[{}A", n_channels + 1);
                     }
+                    // @TODO check here, at the end the channels shouldn't be a problem here, at
+                    // the end all data can be written to the SPI and done.
                     println!("captured {} samples", n_samples / n_channels);
                     for c in 0..n_channels {
                         let mut max: f32 = 0.0;
@@ -90,6 +123,15 @@ pub fn main() -> Result<(), pw::Error> {
                             let start = n as usize * mem::size_of::<f32>();
                             let end = start + mem::size_of::<f32>();
                             let chan = &samples[start..end];
+
+                            //@TODO: Should we write chan by chan or all the sample to the SPI?
+                            match writer.write(chan){
+                                Ok(x) => x,
+                                Err(_) => {
+                                    println!("Failed to write to SPI");
+                                    0
+                                },
+                            };
                             let f = f32::from_le_bytes(chan.try_into().unwrap());
                             max = max.max(f.abs());
                         }
